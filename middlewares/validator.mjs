0397083bb919a32b3a53e43e4889ec245c017f43
Iicytower <szymonfirkowicz@gmail.com>
2020-09-15 14:11:23 +0200

    add middlewares

---
+++ middlewares/validator.mjs
@@ -0,0 +1,18 @@
+import { validationResult } from "express-validator";
+
+function validator() {
+    return (req, res, next) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).json({errors: errors.array()});
+            }
+
+            next();
+        } catch (err) {
+            throw err;
+        }
+    };
+}
+
+export default validator;
